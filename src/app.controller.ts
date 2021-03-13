50858c28746323ee69a4dc65c6624f7d7c0937be
brian <brian@toimc.com>
2021-03-14 00:51:15 +0800

    使用config库

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
-import { DatabaseConfig } from './interface';
+import * as config from 'config';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    const db = this.configService.get<DatabaseConfig>('db');
-    console.log(db);
+    const server = config.get('server');
+    console.log(server);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import Configuration from './config/configuration';
-
-// const envPath = `.env.${process.env.NODE_ENV || 'development'}`;
-// console.log('🚀 ~ file: app.module.ts ~ line 7 ~ envPath', envPath);
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      // envFilePath: envPath,
-      load: [Configuration],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
